931d61212989929a2c4986c44d0064077ff3633d
Rohan-San <rohansanthosh02@gmail.com>
2024-08-08 17:17:54 +0530

    removed api key rerouting and used the key directly

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import {RAPID_API_KEY} from '@env';
-
-const rapidApiKey = RAPID_API_KEY
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
@@ -13,9 +10,37 @@ const useFetch = (endpoint, query) => {
     method: 'GET',
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      'x-rapidapi-key': rapidApiKey,
+      'x-rapidapi-key': '011ed3ba0emsh24548118f51dc09p11a04ejsn363973b6bd89',
       'x-rapidapi-host': 'jsearch.p.rapidapi.com'
     },
     params: { ...query }
   };
-}
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
